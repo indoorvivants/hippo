1919ee0b9b383ed9a6eacb11fb396d8524e09638
Anton Sviridov <keynmol@gmail.com>
2022-08-24 12:53:47 +0100

    Encode Long as String to work around issue on JS

--- modules/shared/src/main/scala/models.scala
+++ modules/shared/src/main/scala/models.scala
@@ -42,8 +42,8 @@ object Identifier:
   extension (d: Identifier) def value: Long = d
   inline def from(id: Long): Identifier     = id
 
-  private val enc: Encoder[Identifier] = Encoder.encodeLong
-  private val dec: Decoder[Identifier] = Decoder.decodeLong
+  private val enc: Encoder[Identifier] = Encoder.encodeString.contramap(_.toString)
+  private val dec: Decoder[Identifier] = Decoder.decodeString.map(_.toLong)
   given Codec[Identifier]              = Codec.from(dec, enc)
 
 opaque type ClassSerialNumber = Int
